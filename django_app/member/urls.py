a42e821f4c1e17581d2e579977c700a8b65ac97d
Lee HanYeong <dev@azelf.com>
2017-04-08 17:17:43 +0900

    login template, url연결

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'member'
 urlpatterns = [
-    url(r'^login/$', views.login_view),
+    url(r'^login/$', views.login_view, name='login'),
 ]
