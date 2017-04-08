121af8e1eebfd7e21e353d9cd26adc59e4a74fc9
Lee HanYeong <dev@azelf.com>
2017-04-08 14:26:43 +0900

    First commit, Product models 구현

---
+++ django_app/product/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=64)
+    parent = models.ForeignKey('self', null=True)
+    priority = models.IntegerField(default=0)
+
+    def __str__(self):
+        return '{:03} | {}'.format(
+            self.priority,
+            self.title
+        )
+
+    class Meta:
+        index_together = (
+            'parent', 'priority'
+        )
+
+
+class Product(models.Model):
+    categories = models.ManyToManyField(Category)
+    title = models.CharField(max_length=64)
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='product', blank=True)
+    description = models.TextField(blank=True)
+    inventory_count = models.IntegerField(default=0)
+    score = models.FloatField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class DateRangeDiscount(models.Model):
+    category = models.ForeignKey(Category)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    rate = models.FloatField()
+
+    def __str__(self):
+        return 'Category({category}) {rate:.1%}, [{start_date} - {end_date}]'.format(
+            category=self.category.title,
+            rate=self.rate,
+            start_date=self.start_date,
+            end_date=self.end_date
+        )
